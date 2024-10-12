ad5b30ecb15fc50bcb7aeacfdecdfb91c0ee7d6f
adhikariabhinav07 <128512549+adhikariabhinav07@users.noreply.github.com>
2024-10-11 21:41:13 -0500

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -8,24 +8,21 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
 data_train = pd.read_csv("https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3.csv")
-data_test = pd.read_csv("https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3test.csv")
-
-model_t = DecisionTreeClassifier(max_depth=5,min_samples_leaf=10)
-y_t = data_test['meal']
-x_t = data_test.drop(['meal','id','DateTime'], axis=1).dropna()
-
-x_t, xt, y_t, yt = train_test_split(x_t, y_t, test_size=0.33, random_state=42)
-pred_t = model_t.fit(x_t,y_t)
-print("\n\nout-sample accuracy: %s%%\n\n" 
- % str(round(100*accuracy_score(y_t, model.predict(x_t)), 2)))
-
+y_t = data_train['meal']
+x_t = data_train.drop(['meal','id','DateTime'], axis=1).dropna()
+X, xt, Y, yt = train_test_split(x_t, y_t, test_size=0.33, random_state=42)
 model = DecisionTreeClassifier(max_depth=5,min_samples_leaf=10)
+modelFit = model.fit(X,Y)
 
-y = data_train['meal']
-x = data_train.drop(['meal','id','DateTime'], axis=1).dropna()
-pred = model.fit(x,y)
+print(f"\n\nIn-sample accuracy: {round(100 * accuracy_score(Y, model.predict(X)), 2)}%\n\n")
+print(f"\n\nOut-of-sample accuracy: {round(100 * accuracy_score(yt, model.predict(xt)), 2)}%\n\n")
 
-print("\n\nIn-sample accuracy: %s%%\n\n" 
- % str(round(100*accuracy_score(y, model.predict(x)), 2)))
+
+data_test = pd.read_csv("https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3test.csv")
+test = data_test.drop(["meal","id","DateTime"],axis = 1)
+pred = model.predict(test)
+
+
+
 
 
