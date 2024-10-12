a801eae6c777018f3187fb7745f08abf59ccbd87
adhikariabhinav07 <128512549+adhikariabhinav07@users.noreply.github.com>
2024-10-11 22:02:06 -0500

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -10,7 +10,8 @@ from sklearn.model_selection import train_test_split
 data_train = pd.read_csv("https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3.csv")
 y_t = data_train['meal']
 x_t = data_train.drop(['meal','id','DateTime'], axis=1).dropna()
-X, xt, Y, yt = train_test_split(x_t, y_t, test_size=0.33, random_state=42)
+
+X, xt, Y, yt = train_test_split(x_t, y_t, test_size=0.30, random_state=42)
 model = DecisionTreeClassifier(max_depth=5,min_samples_leaf=10)
 modelFit = model.fit(X,Y)
 
@@ -22,10 +23,7 @@ data_test = pd.read_csv("https://github.com/dustywhite7/Econ8310/raw/master/Assi
 test = data_test.drop(["meal","id","DateTime"],axis = 1)
 pred = model.predict(test)
 
-# store predictions
 
-data_test["what_meal"] = pred
-data_test(["id","what_meal"]).to_csv("where.csv",index = False)
 
 
 
